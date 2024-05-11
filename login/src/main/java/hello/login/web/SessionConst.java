72662403126c6305cbc42d3af5c501bec8640db8
Raedeok-H <raedeokhwang@gmail.com>
2024-05-12 00:15:34 +0900

    로그인 처리하기 - 서블릿 HTTP 세션1

---
+++ login/src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,14 @@
+package hello.login.web;
+
+public class SessionConst {
+    public static String LOGIN_MEMBER = "loginMember";
+}
+
+/*
+상수처럼 가져다가 쓰려면 아래처럼 하는 것이 낫다. 또는 abstract class
+학습에선 그냥 위에처럼 진행한다.
+package hello.login.web;
+
+public interface SessionConst {
+    String LOGIN_MEMBER = "loginMember";
+}*/
