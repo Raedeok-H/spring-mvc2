920e1a4128d7a10065bdd1ebf074e2f8836c1184
Raedeok-H <raedeokhwang@gmail.com>
2024-05-11 12:24:20 +0900

    Bean Validation - 스프링 적용

--- validation/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ validation/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,20 +1,12 @@
 package hello.itemservice;
 
-import hello.itemservice.web.validation.ItemValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.validation.Validator;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ItemServiceApplication{ //implements WebMvcConfigurer {
-								    // 글로벌 설정할 때
+public class ItemServiceApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
-
-//	@Override
-//	public Validator getValidator() {
-//		return new ItemValidator();
-//	}
+// 애노테이션 기반 빈 검증을 위해 글로벌 설정 제거
 }
