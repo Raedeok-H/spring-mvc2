ba403dcf3faa8f725259f287a84117a22836c45b
Raedeok-H <raedeokhwang@gmail.com>
2024-05-12 11:12:01 +0900

    서블릿 필터 - 요청 로그

---
+++ login/src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,20 @@
+package hello.login;
+
+import hello.login.web.filter.LogFilter;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1); // 필터는 체인으로 동작한다. 따라서 순서가 필요하다. 낮을 수록 먼저 동작한다.
+        filterRegistrationBean.addUrlPatterns("/*"); // 모든 URL 에 적용
+
+        return filterRegistrationBean;
+    }
+}
