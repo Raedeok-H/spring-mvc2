5af70dbd74f26540aed7816097ab799ad98b77c8
Raedeok-H <raedeokhwang@gmail.com>
2024-05-20 21:58:38 +0900

    서블릿과 파일 업로드1

---
+++ upload/src/main/java/hello/upload/controller/ServletUploadControllerV1.java
@@ -0,0 +1,39 @@
+package hello.upload.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.Part;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.util.Collection;
+
+@Slf4j
+@Controller
+@RequestMapping("/servlet/v1")
+public class ServletUploadControllerV1 {
+    @GetMapping("/upload")
+    public String newFile() {
+        return "upload-form";
+    }
+
+    @PostMapping("/upload")
+    public String saveFile(HttpServletRequest request) throws ServletException, IOException {
+        log.info("request={}", request);
+
+        String itemName = request.getParameter("itemName");
+        log.info("itemName={}", itemName);
+
+        Collection<Part> parts = request.getParts();
+        log.info("parts={}", parts);
+
+        return "upload-form";
+    }
+    // Content-Type: multipart/form-data; boundary=----WebKitFormBoundary8quh2Ng6IIbmNrgL
+    // 위와 같은 결과를 확인할 수 있다(trace로 로그를 세팅해야함)
+
+}
